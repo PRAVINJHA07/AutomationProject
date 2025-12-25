f9ab1f2984d0a12211f416a138e41a0ac2bdbe6f
Pravin <pravinkrjha07@gmail.com>
2025-12-26 00:19:45 +0530

    New Line Code

--- src/demo/Program1.java
+++ src/demo/Program1.java
@@ -5,6 +5,8 @@ public class Program1 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Team New Code by Pravin");
+		
+		System.out.println("Hello Sir , I have added new line ");
 	}
 
 }
